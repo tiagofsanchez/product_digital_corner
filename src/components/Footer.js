5ecde5a5851362d75edad5e7fb2ea401c3219190
tiagofsanchez <tiagofsanchez@gmail.com>
2022-11-04 11:49:13 +0000

    Link to my digital garden

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,17 @@ const containerStyles = {
 const Footer = () => {
   return (
     <Box sx={containerStyles}>
-      <Typography variant="h6">Made with ❤️ by Tiago</Typography>
+      <Typography variant="h6">
+        Made with ❤️ by{" "}
+        <a
+          href="https://www.tiagofsanchez.com/"
+          target="_blank"
+          aria-label="Go to Tiago's digital garden "
+          style={{ color: `#d23669` }}
+        >
+          Tiago
+        </a>
+      </Typography>
     </Box>
   );
 };
